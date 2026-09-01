8f30ebb4695ffb834a6fa228a958ae16f4fe5032
유섭 <yoonbk2001027@gmail.com>
2026-09-01 19:30:56 +0900

    feat(api): configure public Swagger URL

--- src/main/kotlin/org/example/balogserver/global/config/SwaggerConfig.kt
+++ src/main/kotlin/org/example/balogserver/global/config/SwaggerConfig.kt
@@ -5,11 +5,13 @@ import io.swagger.v3.oas.models.OpenAPI
 import io.swagger.v3.oas.models.info.Info
 import io.swagger.v3.oas.models.security.SecurityRequirement
 import io.swagger.v3.oas.models.security.SecurityScheme
+import io.swagger.v3.oas.models.servers.Server
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
 class SwaggerConfig {
-    @Bean fun openAPI(): OpenAPI { val jwt = "JWT"; return OpenAPI().info(apiInfo()).addSecurityItem(SecurityRequirement().addList(jwt)).components(Components().addSecuritySchemes(jwt, SecurityScheme().name(jwt).type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT"))) }
+    @Bean fun openAPI(@Value("\${app.public-url:}") publicUrl: String): OpenAPI { val jwt = "JWT"; return OpenAPI().info(apiInfo()).addSecurityItem(SecurityRequirement().addList(jwt)).components(Components().addSecuritySchemes(jwt, SecurityScheme().name(jwt).type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT"))).apply { if (publicUrl.isNotBlank()) servers(listOf(Server().url(publicUrl))) } }
     private fun apiInfo() = Info().title("Balog Server API").description("Balog 서비스의 API 명세서입니다.").version("1.0.0")
 }
